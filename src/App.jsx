7514c5b44888ab3ef28084a05848a060a92b06d5
GaranceBou <garanceboussuge@gmail.com>
2024-03-19 12:14:17 +0100

    seventh 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/header";
-import Title from "./components/title";
 import Jobs from "./components/jobs";
 import Footer from "./components/footer";
 
